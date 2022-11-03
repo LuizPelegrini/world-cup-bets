a6268f23f8d0909f4ec7791e4a92a4692d94ce39
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-11-03 22:04:40 +0800

    add: Create pool creation route. Create user count route. Create guess count route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({
   log: ['query'] // logs out all query
@@ -17,6 +19,38 @@ async function bootstrap(){
 
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count();
+
+    return { count };
+  });
+
+  fastify.post('/pools', async (request, response) => {
+    const createBodySchema = z.object({
+      title: z.string()
+    });
+
+    // validate body
+    const { title } = createBodySchema.parse(request.body);
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code
+      }
+    })
+
+    return response.status(201).send({ code });
+  });
+
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count();
 
     return { count };
   });
